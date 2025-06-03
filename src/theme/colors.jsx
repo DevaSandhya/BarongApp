87e98d9ada3081e002393223a1c804445ffb96d6
DevaSandhya <devasandhya24@gmail.com>
2025-06-03 11:56:26 +0700

    [BAB 2] KOMPONEN REACT NATIVE DAN TEXT HANDLING

---
+++ src/theme/colors.jsx
@@ -0,0 +1,9 @@
+export const colors = {
+  white: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  black: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+  grey: (opacity = 1) => `rgba(120, 120, 120, ${opacity})`,
+  redOrange: (opacity = 1) => `rgba(255, 87, 34, ${opacity})`,
+  cream: (opacity = 1) => `rgba(250, 243, 224, ${opacity})`, // #FAF3E0
+};
+
+export default colors;
